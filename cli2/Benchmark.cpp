6f79636b3ee26cd84049adeb798e3f8c96e8430a
Nick Thompson <ncthom91@gmail.com>
2023-02-17 15:02:18 -0500

    Merge benchmark and cli subprojects

---
+++ cli2/Benchmark.cpp
@@ -0,0 +1,103 @@
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <iostream>
+
+#include <choc_Files.h>
+#include <choc_javascript.h>
+#include <choc_javascript_QuickJS.h>
+
+#include "Runtime.h"
+
+
+template <typename FloatType>
+void runBenchmark(std::string const& name, std::string const& inputFile) {
+    elem::Runtime<FloatType> runtime(44100.0, 512);
+
+    auto ctx = choc::javascript::createQuickJSContext();
+
+    ctx.registerFunction("__postNativeMessage__", [&](choc::javascript::ArgumentList args) {
+        runtime.applyInstructions(elem::js::parseJSON(args[0]->toString()));
+        return choc::value::Value();
+    });
+
+    ctx.registerFunction("__log__", [](choc::javascript::ArgumentList args) {
+        for (size_t i = 0; i < args.numArgs; ++i) {
+            std::cout << choc::json::toString(*args[i], true) << std::endl;
+        }
+
+        return choc::value::Value();
+    });
+
+    // Execute the input file
+    auto rv = ctx.evaluate(inputFile);
+
+    // Now we benchmark the realtime processing step
+    std::vector<std::vector<FloatType>> scratchBuffers;
+    std::vector<FloatType*> scratchPointers;
+
+    for (int i = 0; i < 2; ++i) {
+        scratchBuffers.push_back(std::vector<FloatType>(512));
+        scratchPointers.push_back(scratchBuffers[i].data());
+    }
+
+    // Run the first block to process the events
+    runtime.process(
+        nullptr,
+        0,
+        scratchPointers.data(),
+        2,
+        512,
+        0
+    );
+
+    // Now we can measure the static render process. We have this sleep
+    // here to clearly demarcate, in the profiling timeline, which work is
+    // related to the event processing above and which work is related to this
+    // work loop here
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::vector<double> deltas;
+
+    for (size_t i = 0; i < 10'000; ++i) {
+        auto t0 = std::chrono::steady_clock::now();
+
+        runtime.process(
+            nullptr,
+            0,
+            scratchPointers.data(),
+            2,
+            512,
+            0
+        );
+
+        auto t1 = std::chrono::steady_clock::now();
+        auto diffms = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+        deltas.push_back(diffms);
+    }
+
+    // Reporting
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto const sum = std::accumulate(deltas.begin(), deltas.end(), 0.0);
+    auto const avg = sum / (double) deltas.size();
+
+    std::cout << "[Running " << name << "]:" << std::endl;
+    std::cout << "Total run time: " << sum << "us " << "(" << (sum / 1000000) << "s)" << std::endl;
+    std::cout << "Average iteration time: " << avg << "us" << std::endl;
+    std::cout << "Done" << std::endl << std::endl;
+}
+
+
+int main(int argc, char** argv) {
+    // Read the input file from disk
+    if (argc < 2) {
+        std::cout << "Missing argument: what file do you want to run?" << std::endl;
+        return 1;
+    }
+
+    auto inputFile = choc::file::loadFileAsString(argv[1]);
+
+    runBenchmark<float>("Float", inputFile);
+    runBenchmark<double>("Double", inputFile);
+
+    return 0;
+}
