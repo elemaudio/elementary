34fe7e7d0168151c43578bb218392723516ea86a
Nick Thompson <ncthom91@gmail.com>
2023-07-10 10:14:19 -0400

    Much better thread safety for capture node

--- runtime/builtins/Capture.h
+++ runtime/builtins/Capture.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <atomic>
-#include <memory>
-
 #include "../GraphNode.h"
 #include "../Invariant.h"
-#include "../SingleWriterSingleReaderQueue.h"
+#include "../MultiChannelRingBuffer.h"
 
-#include "helpers/RefCountedPool.h"
+#include "helpers/Change.h"
 
 
 namespace elem
@@ -16,30 +13,9 @@ namespace elem
     template <typename FloatType>
     struct CaptureNode : public GraphNode<FloatType> {
         CaptureNode(NodeId id, FloatType const sr, int const blockSize)
-            : GraphNode<FloatType>::GraphNode(id, sr, blockSize)
+            : GraphNode<FloatType>::GraphNode(id, sr, blockSize),
+              ringBuffer(1, 4 * blockSize)
         {
-            setProperty("size", js::Value((js::Number) blockSize));
-        }
-
-        void setProperty(std::string const& key, js::Value const& val) override
-        {
-            if (key == "size") {
-                invariant(val.isNumber(), "size prop for loopcap node must be a number type.");
-
-                auto size = static_cast<size_t>((js::Number) val);
-
-                // First drop any buffer references we're holding here on the main thread
-                std::atomic_store(&relay, std::shared_ptr<BufferType>(nullptr));
-
-                // Next we go resize everything that we can
-                bufferPool.forEach([&](std::shared_ptr<BufferType>& buffer) {
-                    if (buffer.use_count() == 1) {
-                        buffer->resize(size);
-                    }
-                });
-            }
-
-            GraphNode<FloatType>::setProperty(key, val);
         }
 
         void process (BlockContext<FloatType> const& ctx) override {
@@ -57,64 +33,55 @@ namespace elem
             std::copy_n(inputData[1], numSamples, outputData);
 
             // Capture
+            size_t writeStart = 0;
+            size_t writeEnd = 0;
+
             for (size_t i = 0; i < numSamples; ++i) {
                 auto const g = static_cast<bool>(inputData[0][i]);
-                auto const xn = inputData[1][i];
+                auto const dg = change(inputData[0][i]);
 
-                // If the record signal is low and there's nothing to relay, skip ahead
-                if (!g && captureBuffer == nullptr)
-                    continue;
+                // If we're at the rising edge of the gate signal, mark the write start
+                if (dg > FloatType(0.5))
+                    writeStart = i;
 
-                // If the record signal is low and we have something to relay, do it
-                if (!g && captureBuffer != nullptr) {
-                    std::atomic_store(&relay, std::move(captureBuffer));
-                    captureBuffer = nullptr;
-                    continue;
+                // If we're recording, update the write end
+                if (g && std::abs(dg) < FloatType(0.1))
+                    writeEnd = i;
+
+                // If we're at the falling edge of the gate signal, commit the recording
+                if (dg < FloatType(-0.5)) {
+                    ringBuffer.write(inputData, 1, writeEnd - writeStart);
+                    relayReady.store(true);
+
+                    writeStart = 0;
+                    writeEnd = 0;
                 }
+            }
 
-                // If the record signal is high, we try to capture, grabbing a new buffer
-                // to record into if we don't yet have one
-                if (g) {
-                    if (captureBuffer == nullptr) {
-                        captureBuffer = bufferPool.allocateAvailableWithDefault(nullptr);
-
-                        // If captureBuffer is still nullptr, the allocation failed, and there's
-                        // nothing we can do right now
-                        if (captureBuffer == nullptr)
-                            continue;
-
-                        // Else, we reset our write pos and carry on
-                        writePos = 0;
-                    }
-
-                    if (writePos < captureBuffer->size()) {
-                        auto* captureData = captureBuffer->data();
-                        captureData[writePos++] = xn;
-                    }
-                }
+            if (writeEnd - writeStart > 0) {
+                ringBuffer.write(inputData, 1, writeEnd - writeStart);
             }
         }
 
         void processEvents(std::function<void(std::string const&, js::Value)>& eventHandler) override {
-            js::Float32Array copy;
+            auto const samplesAvailable = ringBuffer.size();
 
-            if (auto relayBuffer = std::atomic_exchange(&relay, std::shared_ptr<BufferType>(nullptr)))
-            {
-                auto sizeProp = GraphNode<FloatType>::getPropertyWithDefault("size", (js::Number) 0);
-                auto expectedSize = static_cast<size_t>(sizeProp);
+            if (samplesAvailable > 0) {
+                auto currentSize = relayBuffer.size();
+                relayBuffer.resize(currentSize + samplesAvailable);
 
-                // If we land in here, it's likely because we got a new size prop in setProperty but
-                // couldn't resize the buffer that the realtime thread was holding at the time. We're
-                // now looking at that buffer and it doesn't match the expected size, so we resize it
-                // and skip the relay
-                if (expectedSize > 0 && relayBuffer->size() != expectedSize) {
-                    relayBuffer->resize(expectedSize);
+                auto relayData = relayBuffer.data() + currentSize;
+
+                if (!ringBuffer.read(&relayData, 1, samplesAvailable))
                     return;
-                }
+            }
 
+            if (relayReady.exchange(false)) {
                 // Now we can go ahead and relay the data
-                auto relaySize = relayBuffer->size();
-                auto relayData = relayBuffer->data();
+                js::Float32Array copy;
+
+                auto relaySize = relayBuffer.size();
+                auto relayData = relayBuffer.data();
 
                 copy.resize(relaySize);
 
@@ -123,6 +90,8 @@ namespace elem
                     copy[i] = static_cast<float>(relayData[i]);
                 }
 
+                relayBuffer.clear();
+
                 eventHandler("capture", js::Object({
                     {"source", GraphNode<FloatType>::getPropertyWithDefault("name", js::Value())},
                     {"data", copy}
@@ -130,13 +99,10 @@ namespace elem
             }
         }
 
-        using BufferType = std::vector<FloatType>;
-        RefCountedPool<BufferType> bufferPool;
-
-        std::shared_ptr<BufferType> captureBuffer;
-        size_t writePos = 0;
-
-        std::shared_ptr<BufferType> relay;
+        Change<FloatType> change;
+        MultiChannelRingBuffer<FloatType> ringBuffer;
+        std::vector<FloatType> relayBuffer;
+        std::atomic<bool> relayReady = false;
     };
 
 } // namespace elem
