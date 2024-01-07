ed985303caf052d36c69124cf17a2c2a3870d04a
Leo Mozoloa <leo.drrty@gmail.com>
2024-01-07 20:37:13 +0100

    Splitting fix & softknee into two compress functions (compress & skcompress)

--- js/packages/core/lib/dynamics.ts
+++ js/packages/core/lib/dynamics.ts
@@ -21,8 +21,63 @@ const el = {
   ...si,
 };
 
-/* A simple compressor with parameterized attack and release times,
- * threshold, compression ratio and (soft) knee width.
+/* A simple hard knee compressor with parameterized attack and release times,
+ * threshold, and compression ratio.
+ *
+ * Users may drive the compressor with an optional sidechain signal, or send the
+ * same input both as the input to be compressed and as the sidechain signal itself
+ * for standard compressor behavior.
+ *
+ * @param {Node | number} atkMs – attack time in milliseconds
+ * @param {Node | number} relMs – release time in millseconds
+ * @param {Node | number} threshold – decibel value above which the comp kicks in
+ * @param {Node | number} ratio – ratio by which we squash signal above the threshold
+ * @param {Node} sidechain – sidechain signal to drive the compressor
+ * @param {Node} xn – input signal to filter
+ */
+export function compress(
+  attackMs: ElemNode,
+  releaseMs: ElemNode,
+  threshold: ElemNode,
+  ratio: ElemNode,
+  sidechain: ElemNode,
+  xn: ElemNode,
+): NodeRepr_t;
+
+export function compress(a_, b_, c_, d_, e_, f_, g_?) {
+  let children = (typeof a_ === "number" || isNode(a_))
+    ? [a_, b_, c_, d_, e_, f_,]
+    : [b_, c_, d_, e_, f_, g_,];
+
+  const [atkMs, relMs, threshold, ratio, sidechain, xn] = children;
+  const env = el.env(
+    el.tau2pole(el.mul(0.001, atkMs)),
+    el.tau2pole(el.mul(0.001, relMs)),
+    sidechain,
+  );
+
+  const envDecibels = el.gain2db(env);
+
+  // Calculate gain multiplier from the ratio (1 - 1/ratio)
+  const adjustedRatio = el.sub(1, el.div(1, ratio));
+
+  // Calculate gain reduction in dB
+  const gain = el.mul(adjustedRatio, el.sub(threshold, envDecibels)) // adjustedRatio * (threshold - envDecibels);
+
+  // Ensuring gain is not positive
+  const cleanGain = el.min(0, gain);
+
+  // Convert the gain reduction in dB to a gain factor
+  const compressedGain = el.db2gain(cleanGain);
+
+  return el.mul(xn, compressedGain);
+}
+
+
+/* A simple softknee compressor with parameterized attack and release times,
+ * threshold, compression ratio and knee width. 
+ *
+ * Functions like regular el.compress when kneeWidth is 0.
  *
  * Users may drive the compressor with an optional sidechain signal, or send the
  * same input both as the input to be compressed and as the sidechain signal itself
@@ -36,17 +91,7 @@ const el = {
  * @param {Node} sidechain – sidechain signal to drive the compressor
  * @param {Node} xn – input signal to filter
  */
-export function compress(
-  attackMs: ElemNode,
-  releaseMs: ElemNode,
-  threshold: ElemNode,
-  ratio: ElemNode,
-  kneeWidth: ElemNode,
-  sidechain: ElemNode,
-  xn: ElemNode,
-): NodeRepr_t;
-
-export function compress(
+export function skcompress(
   props: OptionalKeyProps,
   attackMs: ElemNode,
   releaseMs: ElemNode,
@@ -57,7 +102,7 @@ export function compress(
   xn: ElemNode,
 ): NodeRepr_t;
 
-export function compress(a_, b_, c_, d_, e_, f_, g_, h_?) {
+export function skcompress(a_, b_, c_, d_, e_, f_, g_, h_?) {
   let children = (typeof a_ === "number" || isNode(a_))
     ? [a_, b_, c_, d_, e_, f_, h_]
     : [b_, c_, d_, e_, f_, g_, h_];
@@ -81,7 +126,7 @@ export function compress(a_, b_, c_, d_, e_, f_, g_, h_?) {
     el.leq(envDecibels, upperKneeBound), // envDecibels <= upperKneeBound
   );
 
-  // Calculate gain multiplier for the ratio (1 - 1/ratio)
+  // Calculate gain multiplier from the ratio (1 - 1/ratio)
   const adjustedRatio = el.sub(1, el.div(1, ratio));
 
   /* Gain calculation
@@ -93,7 +138,7 @@ export function compress(a_, b_, c_, d_, e_, f_, g_, h_?) {
   const gain = el.select(
     isInSoftKneeRange,
     el.mul(
-      el.mul(adjustedRatio, 0.5),
+      el.div(adjustedRatio, 2),
       el.mul(
         el.div(el.sub(envDecibels, lowerKneeBound), kneeWidth),
         el.sub(lowerKneeBound, envDecibels)
