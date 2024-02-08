0d2693e4d627e2842c27b6d19cfe8117655b7631
Nick Thompson <ncthom91@gmail.com>
2024-02-08 09:22:09 -0500

    Update web-renderer test infra to vitest

--- js/packages/web-renderer/test/vite.config.js
+++ js/packages/web-renderer/test/vite.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   optimizeDeps: {
     include: ['@elemaudio/core'],
+    force: true,
+  },
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chrome',
+      headless: true,
+    },
   },
 })
