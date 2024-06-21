cf09c1da1c84708beab0dfdfa82b8ba0bcd19f9c
Daniel Zerlett <daniel@zerlett.eu>
2024-06-21 15:40:36 +0200

    Add docker.io prefix to emscripten image to make it work with podman

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -60,7 +60,7 @@ el__main() {
         docker run \
           -v $(pwd):/src \
           --env ELEM_BUILD_ASYNC="$ELEM_BUILD_ASYNC" \
-          emscripten/emsdk:3.1.52 \
+          docker.io/emscripten/emsdk:3.1.52 \
           ./scripts/build-wasm.sh build
 
         # Then we copy the resulting file over to the website directory where
